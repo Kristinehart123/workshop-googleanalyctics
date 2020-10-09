eb7ec07c6f2005657d3437f822106fd4364ac644
Kristinehart123 <56019654+Kristinehart123@users.noreply.github.com>
2020-10-09 15:08:36 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+window.addEventListener('wheel', handleScroll);
+
+
+function handleScroll(event) {
+    event.deltaY;
+    let productElm = document.querySelector('.product-demo');
+    productElm.style.cssText = "border: 5px solid black;"
+}
